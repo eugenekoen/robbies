5af2aa5fe9bbc4a825f923d82214aed40f7933cc
eugenekoen <56178255+eugenekoen@users.noreply.github.com>
2020-06-15 13:02:03 +0200

    Updates

---
+++ precache-manifest.e61f50b7c55ae6cca16a96662e763b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1d7e82afdb9b1a1675a310e577c03b",
+    "url": "/robbies/index.html"
+  },
+  {
+    "revision": "a34b09fe9f56c209f65a",
+    "url": "/robbies/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "852d2f440af0919ef563",
+    "url": "/robbies/static/js/2.4bee88c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robbies/static/js/2.4bee88c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34b09fe9f56c209f65a",
+    "url": "/robbies/static/js/main.02f9dfca.chunk.js"
+  },
+  {
+    "revision": "37b6c2c2129328e98da0",
+    "url": "/robbies/static/js/runtime-main.a397c1b1.js"
+  }
+]);
